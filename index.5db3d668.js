33a39e21fa3718f9b0649eb57f26c6c2bf3db864
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-09-26 19:03:18 +0300

    make build

---
+++ index.5db3d668.js
@@ -0,0 +1,2 @@
+var t,e,l,c,i=document.querySelector(".wall"),o=document.querySelector(".spider");t=i.clientWidth,e=i.clientHeight,l=o.clientWidth,c=o.clientHeight,o.style.position="absolute",o.style.left="".concat((t-l)/2,"px"),o.style.top="".concat((e-c)/2,"px");
+//# sourceMappingURL=index.5db3d668.js.map
